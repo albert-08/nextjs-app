1a235c7bb896e5f7e62728d56d60e86788272dc3
Alberto Flores <alberto.floreso94@gmail.com>
2021-03-18 10:00:40 -0600

    last commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
+import { useState, useEffect } from 'react'
 
 const HomePage = () => {
+  const [productList, setProductList] = useState<TProduct[]>([])
+
+  useEffect(() => {
+    window
+      .fetch('/api/avo')
+      .then((response) => response.json())
+      .then(({data, length}) => {
+        setProductList(data)
+      })
+  }, [])
+
   return (
     <div>
-      <Navbar />
-      <div>Platzi and Next.js!</div>
+      <div>React and Next.js!</div>
+      {productList.map(product => (
+        <div>{ product.name }</div>
+      ))}
     </div>
   )
 }
